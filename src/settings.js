6a8b258e437a6c03415008df15afb94511219d0a
Naren Sulegai <86naren@gmail.com>
2019-08-26 13:32:51 +0530

    Construct default content data

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export const content = {
   name: 'main',
   container: 'row',
@@ -15,7 +17,7 @@ export const content = {
             widget: 'NonEmptyInputBox',
             options: {label: 'Name'},
             hideIf: (data) => {
-              return !data.main[0];
+              return data.main[0] === null;
             }
           }
         ]
@@ -24,6 +26,29 @@ export const content = {
   ]
 };
 
-export const data = {
-  main: ['', {columns: ['def']}]
+const defaultData = (content) => {
+  let data = {};
+  const initContent = (level, content) => {
+    const name = content.name;
+    content.items.forEach((item, index) => {
+      const l = [...level, name, index];
+      _.set(data, l.join('.'), null);
+      if (item.content) {
+        initContent(l, item.content)
+      }
+    });
+  };
+  initContent([], content);
+  return data;
 };
+
+const mergeWithDefaultData = (data = {}) => {
+  return _.mergeWith(defaultData(content), data, (objValue, srcValue, key, object, source, stack) => {
+  });
+};
+
+const dataFromDB = {
+  main: [null]
+};
+
+export const data = mergeWithDefaultData(dataFromDB);
