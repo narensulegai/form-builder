ed8d02eaaf30d89d17ef0b8066de1f13ab1e2557
Naren Sulegai <86naren@gmail.com>
2022-01-19 11:13:05 -0800

    Drop down to select config files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
-import {content} from './services/settings/tenants/tenant2';
+import {content as content1} from './services/settings/tenants/tenant1';
+import {content as content2} from './services/settings/tenants/tenant2';
+// import {content as content3} from './services/settings/tenants/tenant3';
+import {content as content4} from './services/settings/tenants/tenant4';
+import {content as content5} from './services/settings/tenants/tenant5';
+import {content as content6} from './services/settings/tenants/tenant6';
 import './App.scss';
-
+const tenants = [content1, content2, content4, content5, content6];
 const data = window.sessionStorage.getItem('data');
 // const data = null;
-const dataFromDB = data === null ? {} : JSON.parse(data);
+// const dataFromDB = data === null ? {} : JSON.parse(data);
 
 function App() {
 
@@ -14,9 +19,9 @@ function App() {
   const onChange = (t) => {
     data = t;
   };
-
+  const [currTenant, setCurrTenant] = useState(tenants[0]);
   const handleOnSave = () => {
-    console.log(data);
+    alert(JSON.stringify(data, null, 2));
     if (data === null) return;
     const s = JSON.stringify(data);
     window.sessionStorage.setItem('data', s);
@@ -26,17 +31,34 @@ function App() {
     window.sessionStorage.clear();
   };
 
+  const handleTenantChange = (e) => {
+    setCurrTenant(tenants[parseInt(e.target.value)])
+  }
+
+  const handleShowConfig = () => {
+      alert(JSON.stringify(currTenant, null, 2))
+  }
+
   return (
     <div className="app">
       <div className="appContainer">
-
-        <Form content={content}
-              init={dataFromDB}
+        <h1 className="center">JSON Form builder</h1>
+        <h4>
+            The forms below are auto-generated using a json configuration file. Select a sample config file
+            &nbsp;<select onChange={handleTenantChange}>
+                {tenants.map((t,i)=>{
+                    return <option key={i} value={i}>Sample form {i+1}</option>
+                })}
+            </select>&nbsp;
+            <a href="javascript:void(0)" onClick={handleShowConfig}>Show config</a>
+        </h4>
+        <div className="mediumMarginBottom">Enter some data in the form below and click <b>Test Exported From Form</b> to see the JSON data</div>
+        <Form content={currTenant}
+              init={{}}
               onChange={onChange}/>
 
-        <button onClick={handleOnClear}>Clear</button>
-        &nbsp;&nbsp;
-        <button onClick={handleOnSave}>Save</button>
+        {/*<button onClick={handleOnClear}>Clear</button>*/}
+        <button onClick={handleOnSave}>Test Exported From Form</button>
       </div>
     </div>
   );
--- src/services/settings/tenants/tenant2.js
+++ src/services/settings/tenants/tenant2.js
@@ -3,23 +3,23 @@ export const customWidgets = {};
 export const content = {
   container: 'row demark',
   items: [
-    {widget: 'Text', options: {text: 'Welcome to DSCI'}, name: 'intro'},
-    {widget: 'Text', options: {text: 'Where is the building located?'}},
+    {widget: 'Text', options: {text: 'Welcome to this JSON Form builder'}, name: 'intro'},
+    {widget: 'Text', options: {text: 'Where is your house located?'}},
     {widget: 'InputBox', name: 'Location', isRepeatable: true},
     {widget: 'InputBox', name: 'Default', defaultValue: '10'},
-    {widget: 'Text', options: {text: 'Do you own this building?'}, name: 'ownBuilding'},
+    {widget: 'Text', options: {text: 'Do you own this house?'}, name: 'ownBuilding'},
     {widget: 'YesNo', name: 'DoesOwnBuilding'},
-    {widget: 'Text', options: {text: 'What policy do you want to buy'}, name: 'policySelectionQuestion'},
+    {widget: 'Text', options: {text: 'What option do you want to choose'}, name: 'policySelectionQuestion'},
     {
       widget: 'Bop',
       name: 'policyName',
-      options: {options: ['Business Owner Policy', 'Worker Compensation', 'Building Policy']}
+      options: {options: ['Option A', 'Option B', 'Option C']}
     },
     {
       widget: 'Text',
       options: {text: 'Is your building close to police station?'},
-      showOn: "IF(policyName='Business Owner Policy', TRUE, FALSE)",
+      showOn: "IF(policyName='Option A', TRUE, FALSE)",
     },
-    {widget: 'YesNoText', name: 'CloseToPoliceStation', showOn: "IF(policyName='Business Owner Policy', TRUE, FALSE)"}
+    {widget: 'YesNoText', name: 'CloseToPoliceStation', showOn: "IF(policyName='Option A', TRUE, FALSE)"}
   ]
 };
