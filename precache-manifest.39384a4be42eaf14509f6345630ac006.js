080ba4865b0e7899e1d681ad0eb410292b868936
Naren Sulegai <86naren@gmail.com>
2022-01-19 10:29:23 -0800

    Updates

---
+++ precache-manifest.39384a4be42eaf14509f6345630ac006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16b3a5119a990b8d1e931457d11f5a7",
+    "url": "/form-builder/index.html"
+  },
+  {
+    "revision": "6f2d32b4631af6c858a5",
+    "url": "/form-builder/static/css/main.ba0d9c2a.chunk.css"
+  },
+  {
+    "revision": "35dacfb4c2abbbef5fe8",
+    "url": "/form-builder/static/js/2.2961616c.chunk.js"
+  },
+  {
+    "revision": "d1bfca4c622472e2d9407780a8d3da5a",
+    "url": "/form-builder/static/js/2.2961616c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f2d32b4631af6c858a5",
+    "url": "/form-builder/static/js/main.86b3861b.chunk.js"
+  },
+  {
+    "revision": "147dbaf6416cb1817035",
+    "url": "/form-builder/static/js/runtime-main.10a193bf.js"
+  }
+]);
